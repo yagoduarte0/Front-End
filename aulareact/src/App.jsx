c527b2ca95a90279474984a81546d6f6e0845333
yagoduarte0 <yagoduarte1005@gmail.com>
2024-10-17 16:52:21 -0300

    Incluindo aula

--- aulareact/src/App.jsx
+++ aulareact/src/App.jsx
@@ -1,14 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import MyButton from `./components/Header/MyButton`
+import Home from './components/Home'
+import About from './components/About'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <Router>
+      <Routes>
+        <Route path="/Home" element={<Home />} />
+        <Route path="/About" element={<About />} />
+      </Routes>
+    </Router>
+    /*<>
     <div>
       <h1>Welcome to my app</h1>
       <MyButton />
@@ -16,7 +25,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </>*/
   )
 }
 
